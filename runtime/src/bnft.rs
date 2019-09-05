b777fa3e30a59c5fa9a9e369e9c4a8eb79d514d4
Ramshreyas Rao <ramshreyas.rao@gmail.com>
2019-09-05 18:09:08 +0530

    Successfully changes Bnft uri to AccountId. Todo: Verify/Burn Bnft

--- runtime/src/bnft.rs
+++ runtime/src/bnft.rs
@@ -6,58 +6,57 @@ use {system::ensure_signed, timestamp};
 use rstd::prelude::*;
 
 pub trait Trait: balances::Trait + timestamp::Trait {
-    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+  type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
 #[cfg_attr(feature = "std", derive(Debug))]
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
 pub struct BnftClass<Hash, Balance, Moment, AccountId> {
-    name: Hash,
-    total_supply: u64,
-    beneficiary_credential: Hash,
-    verifier_credential: Hash,
-    transfer_bounty: Balance,
-    verification_bounty: Balance,
-    stake: Balance,
-    expiry: Moment,
-    description: Hash,
-    ricardian_contract: Hash,
-    creator: AccountId,
-    created_on: Moment,
+  name: Hash,
+  total_supply: u64,
+  beneficiary_credential: Hash,
+  verifier_credential: Hash,
+  transfer_bounty: Balance,
+  verification_bounty: Balance,
+  stake: Balance,
+  expiry: Moment,
+  description: Hash,
+  ricardian_contract: Hash,
+  creator: AccountId,
+  created_on: Moment,
 }
 
 #[cfg_attr(feature = "std", derive(Debug))]
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
-pub struct Bnft<Hash> {
-    uri: Hash,
-    class_index: u64,
+pub struct Bnft<AccountId> {
+  uri: AccountId,
+  class_index: u64,
 }
 
 decl_storage! {
   trait Store for Module<T: Trait> as Bnft {
     Nonce get(nonce): u64;
     BnftClasses get(get_bnft_class): map u64 => BnftClass<T::Hash, T::Balance, T::Moment, T::AccountId>;
-    Bnfts get(get_bnft): map T::Hash => Bnft<T::Hash>; 
-    BnftIdByOwner get(get_bnft_owner): map T::AccountId => T::Hash;
+    Bnfts get(get_bnft): map T::AccountId => Bnft<T::AccountId>; 
+    BnftIdByOwner get(get_bnft_owner): map T::AccountId => T::AccountId;
     RemainingBnftsForClass get(remaining_bnfts_for): map u64 => u64;
   }
 }
 
 decl_event! {
-    pub enum Event<T> where 
-      AccountId = <T as system::Trait>::AccountId, 
-      Balance = <T as balances::Trait>::Balance,
-      Hash = <T as system::Trait>::Hash,
-      Moment = <T as timestamp::Trait>::Moment,
-    {
-      BnftClassCreated(u64, BnftClass<Hash, Balance, Moment, AccountId>),
-      BnftIssued(AccountId, Bnft<Hash>),  
-    }
+  pub enum Event<T> where 
+    AccountId = <T as system::Trait>::AccountId, 
+    Balance = <T as balances::Trait>::Balance,
+    Hash = <T as system::Trait>::Hash,
+    Moment = <T as timestamp::Trait>::Moment,
+  {
+    BnftClassCreated(u64, BnftClass<Hash, Balance, Moment, AccountId>),
+    BnftIssued(AccountId, Bnft<AccountId>),  
+  }
 }
 
 decl_module! {
   pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-
     fn deposit_event<T>() = default;
 
     fn create_bnft_class(origin, 
@@ -71,89 +70,88 @@ decl_module! {
                          validity: T::Moment,
                          description: T::Hash,
                          ricardian_contract: T::Hash) -> Result {
-        //Ensure signed
-        let sender = ensure_signed(origin)?;
+      //Ensure signed
+      let sender = ensure_signed(origin)?;
 
-        //Ensure name is unique
+      //Ensure name is unique
 
-        //Generate id for new bnft
-        let mut nonce = Self::nonce();
+      //Generate id for new bnft
+      let mut nonce = Self::nonce();
 
-        //Get creation time
-        let now = <timestamp::Module<T>>::get();
+      //Get creation time
+      let now = <timestamp::Module<T>>::get();
 
-        //Calculate expiry
-        let expiry = now.checked_add(&validity).ok_or("Overflow when setting expiry")?;
+      //Calculate expiry
+      let expiry = now.checked_add(&validity).ok_or("Overflow when setting expiry")?;
 
-        //Create struct
-        let bnft_class = BnftClass {
-            name: name.clone(),
-            total_supply: total_supply.clone(),
-            beneficiary_credential,
-            verifier_credential,
-            transfer_bounty,
-            verification_bounty,
-            stake,
-            expiry,
-            description,
-            ricardian_contract,
-            creator: sender.clone(),
-            created_on: now,
-        };
+      //Create struct
+      let bnft_class = BnftClass {
+        name: name.clone(),
+        total_supply: total_supply.clone(),
+        beneficiary_credential,
+        verifier_credential,
+        transfer_bounty,
+        verification_bounty,
+        stake,
+        expiry,
+        description,
+        ricardian_contract,
+        creator: sender.clone(),
+        created_on: now,
+      };
 
-        //Transfer payment for creation
-        
+      //Transfer payment for creation    
 
-        //Save BnftClass, remaining supply, nonce
-        <BnftClasses<T>>::insert(nonce, bnft_class.clone());
-        <RemainingBnftsForClass<T>>::insert(nonce, total_supply);
+      //Save BnftClass, remaining supply, nonce
+      <BnftClasses<T>>::insert(nonce, bnft_class.clone());
+      <RemainingBnftsForClass<T>>::insert(nonce, total_supply);
 
-        //Emit event
-        Self::deposit_event(RawEvent::BnftClassCreated(nonce, bnft_class));
+      //Emit event
+      Self::deposit_event(RawEvent::BnftClassCreated(nonce, bnft_class));
 
-        //Increment nonce
-        nonce = nonce.wrapping_add(1);
-        <Nonce<T>>::put(nonce);
+      //Increment nonce
+      nonce = nonce.wrapping_add(1);
+      <Nonce<T>>::put(nonce);
 
-        Ok(())
+      Ok(())
     }
 
     fn issue_bnft(origin, 
                   class_index: u64, 
-                  uri: T::Hash) -> Result {
-        //Ensure Signed
-        let sender = ensure_signed(origin)?;
+                  uri: T::AccountId) -> Result {
+      //Ensure Signed
+      let sender = ensure_signed(origin)?;
 
-        //Ensure bnft class exists
-        let nonce = Self::nonce();
-        ensure!(class_index < nonce, "BNFT Class does not exist!"); 
+      //Ensure bnft class exists
+      let nonce = Self::nonce();
+      ensure!(class_index < nonce, "BNFT Class does not exist!"); 
 
-        // Ensure uri is unique
-        ensure!(!<Bnfts<T>>::exists(uri), "Bnft already issued");
+      // Ensure uri is unique
+      ensure!(!<Bnfts<T>>::exists(&uri), "Bnft already issued");
 
-        // Ensure beneficiary has correct credential
+      // Ensure beneficiary has correct credential
 
-        //Ensure total supply has not been exceeded
-        let remainingBnftsForClass = Self::remaining_bnfts_for(class_index);
-        ensure!(remainingBnftsForClass > 0, "All BNFTs have been issued for this class");
+      //Ensure total supply has not been exceeded
+      let remainingBnftsForClass = Self::remaining_bnfts_for(class_index);
+      ensure!(remainingBnftsForClass > 0, "All BNFTs have been issued for this class");
 
-        // Transfer stake
+      // Transfer stake
 
-        // Create bnft
-        let bnft = Bnft {
-          uri,
-          class_index,
-        };
+      // Create bnft
+      let bnft = Bnft {
+        uri: uri.clone(),
+        class_index,
+      };
 
-        // Save bnft, assign to owner, decrement remaining supply
-        <Bnfts<T>>::insert(uri.clone(), bnft.clone());
-        <BnftIdByOwner<T>>::insert(sender.clone(), uri.clone());
-        <RemainingBnftsForClass<T>>::insert(class_index, remainingBnftsForClass.clone() - 1);
+      // Save bnft, assign to owner, decrement remaining supply
+      <Bnfts<T>>::insert(uri.clone(), bnft.clone());
+      <BnftIdByOwner<T>>::insert(sender.clone(), uri.clone());
+      <RemainingBnftsForClass<T>>::insert(class_index, remainingBnftsForClass.clone() - 1);
 
-        // Emit event
-        Self::deposit_event(RawEvent::BnftIssued(sender, bnft));
+      // Emit event
+      Self::deposit_event(RawEvent::BnftIssued(sender, bnft));
 
-        Ok(())
+      Ok(())
     } 
   } 
 }
